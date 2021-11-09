025412eb23d8e0f73ed124740cb615acf59e111e
Tayyab <F2017065009@umt.edu.pk>
2021-11-09 15:12:55 +0500

    9Nove2021-2

--- js/catagory.js
+++ js/catagory.js
@@ -4,20 +4,27 @@ const catalougeSM = document.getElementById("catalougeSM");
 const catalougeMM = document.getElementById("catalougeMM");
 const catalougeLG = document.getElementById("catalougeLG");
 
+const catalougeImgContainer = document.querySelectorAll(
+  "#catalougeImgContainer"
+);
+
 const catalouge = document.getElementById("catalouge");
 catalougeSM.addEventListener("click", function () {
-  catalouge.innerHTML =
-    "<div class='catalougeImgContainer'><img src='img/slide1.png' class='catalouge-smImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-sm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide2.png' class='catalouge-smImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-sm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide3.png' class='catalouge-smImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-sm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide4.png' class='catalouge-smImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-sm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide5.png' class='catalouge-smImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-sm'><p style='color: white;'>Led Light</p></div></div>";
+  for (let i = 0; i < catalougeImgContainer.length; i++) {
+    catalougeImgContainer[i].style.width = "12%";
+  }
 });
 
 catalougeMM.addEventListener("click", function () {
-  catalouge.innerHTML =
-    "<div class='catalougeImgContainer'><img src='img/slide1.png' class='catalouge-mmImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-mm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide2.png' class='catalouge-mmImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-mm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide3.png' class='catalouge-mmImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-mm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide4.png' class='catalouge-mmImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-mm'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide5.png' class='catalouge-mmImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content-mm'><p style='color: white;'>Led Light</p></div></div>";
+  for (let i = 0; i < catalougeImgContainer.length; i++) {
+    catalougeImgContainer[i].style.width = "17%";
+  }
 });
 
 catalougeLG.addEventListener("click", function () {
-  catalouge.innerHTML =
-    "<div class='catalougeImgContainer'><img src='img/slide1.png' class='catalouge-lgImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide2.png' class='catalouge-lgImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide3.png' class='catalouge-lgImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide4.png' class='catalouge-lgImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content'><p style='color: white;'>Led Light</p></div></div><div class='catalougeImgContainer'><img src='img/slide5.png' class='catalouge-lgImg' alt=''><p class='nameofCatalouge'><b>LED Light</b></p><div class='catalouge-overlay-content'><p style='color: white;'>Led Light</p></div></div>";
+  for (let i = 0; i < catalougeImgContainer.length; i++) {
+    catalougeImgContainer[i].style.width = "22%";
+  }
 });
 
 const categorySection = document.getElementById("categorySection");
@@ -34,5 +41,7 @@ $(function () {
     categorySection.classList.remove("row");
     categoryList.classList.remove("col-3");
     categoryImages.classList.remove("col-9");
+
+    categoryList.style.display = "none";
   }
 });
